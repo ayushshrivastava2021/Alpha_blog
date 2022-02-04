58becdf0743f6a0b1b5db29d2825e2aa84906f87
Ayush Shrivastava <ayushshrivastava.ror@gmail.com>
2022-02-04 18:28:49 +0530

    Create user profile edit page and fetch data into it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,18 @@ class UsersController < ApplicationController
     def new
       @user = User.new
     end
+    def edit
+      @user = User.find(params[:id]) 
+    end
+
+    def update
+
+    end
 
     def create
       @user = User.new(user_params)
       if @user.save
-        flash[:notice]= "Welcome to Alphablog #{@user.username}, You are successfully signed up "
+         flash[:notice]= "Welcome to Alphablog #{@user.username}, You are successfully signed up "
         redirect_to articles_path
       else
         render 'new'

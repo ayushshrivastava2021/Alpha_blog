73bd550f7287368198c3fe2b2519518933700738
Ayush Shrivastava <ayushshrivastava.ror@gmail.com>
2022-02-07 19:24:04 +0530

    set time on error and notification popup and create user profile and edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
 
+    def show
+      @user  = User.find(params[:id])
+      @articles = @user.articles
+    end
+
+    def index
+      @users  = User.all
+
+    end
+
     def new
       @user = User.new
     end
@@ -8,6 +18,13 @@ class UsersController < ApplicationController
     end
 
     def update
+      @user  = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:notice] = "Your profile successfully updated"
+        redirect_to user_path
+      else 
+        render 'edit'
+      end
 
     end
 
